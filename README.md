328c841fc5ebea01da271bd9e7c9e138d66ce567
Boyan Xu <dominictxu1@gmail.com>
2020-03-02 22:01:32 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Template-Engine
+
+## Description
+Template-Engine is a project that requires us to create a team member and show it on a HTML page. The program will prompt the user different questions by using inquirer. User will choose his/her profession including manager, engineer or intern. Each profession has its own information on the team member. This is a good practice of creating classes and using extends method to do the inheritance.
+
+## Contact:
+Dominic Xu dominictxu1@gmail.com
+
+## Link to deployed App
+[Link to the GitHub page](https://github.com/lorddominic/Good-README-Generator)
+
+## Gif
+![TeamMembers GIF](TeamMembers.gif)
--- app.js
+++ app.js
@@ -7,34 +7,39 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-const members = [];
-inquirer
-    .prompt([{
-        type: "list",
-        message: "What is your profession?",
-        name: "profession",
-        choices: [
-            "manager",
-            "engineer",
-            "intern",
-            "Exit application"
-        ]
-    }])
-    .then(function(response) {
-        switch (response.profession) {
-            case "manager":
-                addManager();
-                break;
-            case "engineer":
-                addEngineer();
-                break;
-            case "intern":
-                addIntern();
-                break;
-            default:
-                exitApplication();
-        }
-    });
+const teamMembers = [];
+
+function initialPrompt() {
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "What type of team member would you like to add?",
+            name: "profession",
+            choices: [
+                "manager",
+                "engineer",
+                "intern",
+                "Exit application"
+            ]
+        }])
+        .then(function(response) {
+            switch (response.profession) {
+                case "manager":
+                    addManager();
+                    break;
+                case "engineer":
+                    addEngineer();
+                    break;
+                case "intern":
+                    addIntern();
+                    break;
+                default:
+                    exitApplication();
+            }
+        });
+};
+
+initialPrompt();
 
 function addManager() {
     inquirer.prompt([{
@@ -58,9 +63,10 @@ function addManager() {
             name: "officeId"
         }
     ]).then(function(res) {
-        var manager = new manager(res.name, res.id, res.email, res.officeId);
-        members.push(manager);
+        var manager = new Manager(res.name, res.id, res.email, res.officeId);
+        teamMembers.push(manager);
         console.log(res);
+        initialPrompt();
         //render(arr);
     })
 }
@@ -87,9 +93,10 @@ function addEngineer() {
             name: "githubName"
         }
     ]).then(function(res) {
-        var engineer = new engineer(res.name, res.id, res.email, res.githubName)
-        members.push(engineer);
+        var engineer = new Engineer(res.name, res.id, res.email, res.githubName)
+        teamMembers.push(engineer);
         console.log(res);
+        initialPrompt();
     })
 }
 
@@ -115,32 +122,20 @@ function addIntern() {
             name: "schoolName"
         }
     ]).then(function(res) {
-        var engineer = new intern(res.name, res.id, res.email, res.schoolName)
-        members.push(intern);
+        var intern = new Intern(res.name, res.id, res.email, res.schoolName)
+        teamMembers.push(intern);
         console.log(res);
+        initialPrompt();
     })
 }
 
 function exitApplication() {
-    for (let i = 0; i < members.length; i++) {
-
-    }
+    var page = render(teamMembers);
+    fs.writeFile(outputPath, page, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!");
+    });
     //loop through team members array,depending on the role,will call display methods and call the html generator. then use fs package to output to the output file
 }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
